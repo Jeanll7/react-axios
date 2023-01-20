d79e89172ccce64e74c3c19cdbea478fb39befc7
devjean <jeancarloleall@gmail.com>
2023-01-20 09:16:26 -0300

    user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import "./App.css";
+import React from "react";
+import axios from "axios";
 
+const baseURL = "https://jsonplaceholder.typicode.com/posts";
 function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  function createPost() {
+    axios
+      .post(baseURL, {
+        title: "Olá Mundo!",
+        body: "Esta é uma nova postagem.",
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  if (!post) return null;
+
   return (
     <div className="App">
-      <div className="container">
-        <h2>React Axios</h2>
-      </div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <button onClick={createPost}>Post</button>
     </div>
   );
 }
