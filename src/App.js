e1743f199e02b06e5798491e86d8ea20ad355cdf
devjean <jeancarloleall@gmail.com>
2023-01-20 20:04:42 -0300

    api

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 import "./App.css";
 import React from "react";
-import axios from "axios";
+// import axios from "axios";
 
-const baseURL = "https://jsonplaceholder.typicode.com/posts";
 function App() {
-  const [post, setPost] = React.useState(null);
-
-  React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
-
-  function createPost() {
-    axios
-      .post(baseURL, {
-        title: "Olá Mundo!",
-        body: "Esta é uma nova postagem.",
-      })
-      .then((response) => {
-        setPost(response.data);
-      });
-  }
-
-  if (!post) return null;
-
   return (
     <div className="App">
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
-      <button onClick={createPost}>Post</button>
+      <h1>React Axios</h1>
     </div>
   );
 }
