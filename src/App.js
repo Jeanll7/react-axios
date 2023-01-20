14f8904e901d625c20be82fe374023b3be3b3df4
devjean <jeancarloleall@gmail.com>
2023-01-20 08:09:36 -0300

    axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import { Outlet } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Navbar />
-        <Outlet />
+        <h2>React Axios</h2>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { createBrowserRouter, RouterProvider, Router } from "react-router-dom";
-import Home from "./routes/Home";
-import NewPost from "./routes/NewPost";
-
 import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/new",
-        element: <NewPost />,
-      },
-    ],
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
