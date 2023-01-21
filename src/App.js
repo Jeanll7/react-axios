b90cde51dd53f2a924e8852d4297baa09ca27604
devjean <jeancarloleall@gmail.com>
2023-01-20 21:34:34 -0300

    constructor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import "./App.css";
 import React from "react";
+import "./App.css";
+import { AiOutlineSearch } from "react-icons/ai";
+import { IoLocationSharp } from "react-icons/io5";
 // import axios from "axios";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Axios</h1>
+    <div className="container">
+      <div className="form">
+        <h3>Confira o clima de uma cidade:</h3>
+        <div className="form-input-container">
+          <input
+            type="text"
+            placeholder="digite o nome da cidade"
+            id="city-input"
+          />
+          <button>
+            <AiOutlineSearch />
+          </button>
+        </div>
+      </div>
+      <div className="weather-data">
+        <h2>
+          <IoLocationSharp />
+          <span id="city"> {""} </span>
+        </h2>
+      </div>
     </div>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "",
+  // https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=
+  // 7ef853c3ea72e45701f6a3c983aa4301
 });
 
 export default api;
